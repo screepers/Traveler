836730e01d279dc4a0b16123e12281a63a0dbf70
SinusQuell <65397804+SinusQuell@users.noreply.github.com>
2023-01-28 19:50:41 +0100

    fixed traveldata memory type

--- Traveler.ts
+++ Traveler.ts
@@ -53,7 +53,7 @@ export class Traveler {
         // initialize data object
         if (!creep.memory._trav) {
             delete creep.memory._travel;
-            creep.memory._trav = {};
+            creep.memory._trav = {} as TravelData;
         }
         let travelData = creep.memory._trav as TravelData;
 
